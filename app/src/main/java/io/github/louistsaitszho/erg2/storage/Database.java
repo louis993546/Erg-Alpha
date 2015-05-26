82079d1ef54951b652f43152500156e7efba19a9
Louis Tsai <louis993546@gmail.com>
2015-05-26 23:21:57 +0800

    Basic implementation of Record and Database

--- app/src/main/java/io/github/louistsaitszho/erg2/storage/Database.java
+++ app/src/main/java/io/github/louistsaitszho/erg2/storage/Database.java
@@ -1,7 +1,29 @@
 package io.github.louistsaitszho.erg2.storage;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Louis on 5/26/2015.
  */
-public class Database {
+public class Database extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String HISTORIES_TABLE_NAME = "Histories";
+    private static final String HISTORIES_TABLE_CREATE = "CREATE TABLE " + HISTORIES_TABLE_NAME + "(" + "Something";    //FIXME not finished yet
+
+    public Database(Context context) {
+        super(context, HISTORIES_TABLE_NAME, null, DATABASE_VERSION);    //TODO check if this is correct
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(HISTORIES_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/io/github/louistsaitszho/erg2/unit/Record.java
+++ app/src/main/java/io/github/louistsaitszho/erg2/unit/Record.java
@@ -1,16 +1,63 @@
 package io.github.louistsaitszho.erg2.unit;
 
+import java.util.GregorianCalendar;
+
 /**
  * Created by Louis on 5/26/2015.
  */
 public class Record {
-    //start date time
-    //duration
+    private GregorianCalendar startTime;    //start date time
+    private double distance; //in meter
     private int rating;
-    //distance
+    private int duration;   //in millisecond
+
+
+
+    //Getter and Setter
 
     public Record() {
-        //TODO default constructor
+        startTime = new GregorianCalendar();    //current date and time
+        distance = 7000;                        //7000m
+        rating = 18;                            //rating 18
+        duration = 1800000;                     //30 minutes
     }
 
+    public Record(GregorianCalendar startTime, int duration, int rating, double distance) {
+        this.startTime = startTime;
+        this.duration = duration;
+        this.rating = rating;
+        this.distance = distance;
+    }
+
+    public GregorianCalendar getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(GregorianCalendar startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }
