f94e2b5227e278f752defd79c714fdf3ce3c1389
Louis Tsai <louis993546@gmail.com>
2015-05-26 22:46:40 +0800

    Initiate

---
+++ app/src/main/java/io/github/louistsaitszho/erg2/storage/Database.java
@@ -0,0 +1,7 @@
+package io.github.louistsaitszho.erg2.storage;
+
+/**
+ * Created by Louis on 5/26/2015.
+ */
+public class Database {
+}
---
+++ app/src/main/java/io/github/louistsaitszho/erg2/unit/Record.java
@@ -0,0 +1,16 @@
+package io.github.louistsaitszho.erg2.unit;
+
+/**
+ * Created by Louis on 5/26/2015.
+ */
+public class Record {
+    //start date time
+    //duration
+    private int rating;
+    //distance
+
+    public Record() {
+        //TODO default constructor
+    }
+
+}
